ddc2e305271185a9ec4fe057c7b6b3b3f0e21f61
fann0128 <yangfanballack@gmail.com>
2017-09-19 13:26:02 +0000

    test vi editor

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    vi test
                 </div>
 
                 <div class="links">
