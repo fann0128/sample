ac0414d40a7b75a4ea89cbf8b6f16a93be1f58e3
vagrant <yangfanballack@gmail.com>
2017-10-19 02:25:34 +0000

    singup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Http\Requests;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
@@ -14,5 +16,27 @@ class UsersController extends Controller
     public function login()
     {
         return view('users.login');
+    }
+
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|min:3|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|min:6|confirmed'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        session()->flash('success', 'Welcome');
+        return redirect()->route('users.show', [$user]);
     }
 }
